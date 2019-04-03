1628f3caa70f03715c403011609301abc83922e1
Nicole Bartholomew <nabartholomew@gmail.com>
2019-04-03 17:29:29 -0500

    (feat) Added javascript interactivity. Initial completion of challenge.

---
+++ challenge_01/script.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function(event){
+	let desktopOptionsMenuNode = document.getElementsByClassName("options__desktop");
+	desktopOptionsMenuNode = desktopOptionsMenuNode[0];
+	// let display = desktopOptionsMenu.getAttribute("display");
+	let buttons = Array.from(document.getElementsByClassName("button"));
+	// let checked = option.getAttribute("aria-checked");
+	// initializing styling on first button selected
+	buttons[0].style.color = "white";
+	buttons[0].style.backgroundColor = "rgb(88, 149, 255)";
+	buttons[0].setAttribute("aria-checked", "true");
+
+	function addClickEvent (node){
+		for(let i = 0; i < buttons.length; i++){
+			let button = buttons[i];
+			// let tab = {node, button};
+			button.addEventListener("click", () => selectOption(button));
+		}
+	}
+
+	function selectOption(selectedOption) {
+		for (let btn of buttons){
+			let selected = btn == selectedOption;
+			btn.style.backgroundColor = selected ? "rgb(88, 149, 255)" : "white";
+			btn.style.color = selected ? "white" : "black";
+
+		}
+	}
+
+	addClickEvent(desktopOptionsMenuNode);
+});
