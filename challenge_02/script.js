624d0a995de1f5494923f1c38ea195bf84b97d39
Nicole Bartholomew <nabartholomew@gmail.com>
2019-04-04 17:07:45 -0500

    (feat) Completed rough solution for interactive tabs.

--- challenge_02/script.js
+++ challenge_02/script.js
@@ -1,26 +1,31 @@
 document.addEventListener("DOMContentLoaded", function(event){
 	let tabsMenuNode = document.getElementsByClassName("tabs")[0];
-	let tabsContent = Array.from(document.getElementsByClassName("tab__content"));
-	// let 
+	let tabsContents = Array.from(document.getElementsByClassName("tab__content"));
+	let wholeTabs = [];
 	let tabs = Array.from(document.getElementsByClassName("tab"));
 
 
 	function connectTabs (node){
-		for(let t of tabs){
-			t.addEventListener("click", () => tabClicked(t));
+		for(let i = 0; i < tabs.length; i++){
+			let tab = tabs[i];
+			let tabsContent = tabsContents[i];
+			let wholeTab = {tabsContent, tab};
+			wholeTabs.push(wholeTab);
+			tab.addEventListener("click", () => tabClicked(wholeTab));
 		}
 	}
 
 
 	function tabClicked (selectedTabObj){
-		for(let tab of tabs){
-			let selected = tab == selectedTabObj;
+		for(let wholeTab of wholeTabs){
+			let selected = wholeTab == selectedTabObj;
 			if(selected){
-				tab.classList.add("tab-active");
-				// tab.node.classList.add("tab__content-active");
+				wholeTab.tab.classList.add("tab-active");
+				wholeTab.tabsContent.classList.add("tab__content-active");
 			}
 			else {
-				tab.classList.remove("tab-active");
+				wholeTab.tab.classList.remove("tab-active");
+				wholeTab.tabsContent.classList.remove("tab__content-active");
 			}
 		}
 	}
