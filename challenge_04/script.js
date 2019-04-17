1674e14afac727fee4af8ac3adbfb32f05f5eb78
Nicole Bartholomew <nabartholomew@gmail.com>
2019-04-17 16:25:23 -0500

    (feat) Initial Commit.

--- challenge_04/script.js
+++ challenge_04/script.js
@@ -3,13 +3,11 @@ document.addEventListener("DOMContentLoaded", function(event){
 	let initButtonArray = Array.from(document.getElementsByClassName("button"));
 	let listArray = Array.from(document.getElementsByClassName("list__item"));
 	let wholeSelectionArray = [];
-	// 
 	let forwardButtonArray = [];
 	let backButtonArray = [];
 	let buttonSelectionOne = [initButtonArray[0]];
 	let buttonSelectionTwo = [initButtonArray[1], initButtonArray[2]];
 	let buttonSelectionThree = [initButtonArray[3], initButtonArray[4]];
-
 	let buttonArray = [buttonSelectionOne, buttonSelectionTwo, buttonSelectionThree];
 	
 	function addListClickEvent (list){
@@ -115,8 +113,6 @@ document.addEventListener("DOMContentLoaded", function(event){
 			let previousLi = listArray[j - 1];
 			let stayActive = wholeSelectionArray[0];
 
-			// if previous item not active dont' do anything
-			
 			if(selected && isActive(previousLi)) {
 					selection.item.classList.add("list__item-active");
 					selection.item.setAttribute("active", "true");
@@ -155,39 +151,7 @@ document.addEventListener("DOMContentLoaded", function(event){
 				
 			}
 		}
-		// if (!isActive(listArray[2]) && !isActive(listArray[1]){
-			// let stayActive = listArray[0];
-			// stayActive.item.classList.add("list__item-active");
-			// stayActive.item.setAttribute("active", "true");
-		// }
 	}
-
 	addListClickEvent(listArray);
 	addButtonClickEvents(initButtonArray);
 });
-
-// function hide(listItem, text, button1, button2) {
-// 		listItem.classList.remove("list__item-active");
-// 		text.classList.remove("submit__content-active");
-// 		button1.classList.remove("button__active");
-// 		button2.classList.remove("button__active");
-// 	}
-
-// 	function display(listItem, text, button1, button2) {
-// 		listItem.classList.add("list__item-active");
-// 		text.classList.add("submit__content-active");
-// 		button1.classList.add("button__active");
-// 		button2.classList.add("button__active");
-// 	}
-
-// 	function clickedLi (selectedLi) {
-// 		for (let i = 0; i < listArray.length; i++) {
-// 			let selected = listArray[i] == selectedLi;
-// 			if(selected) {
-// 				display(listArray[i], submitContentArray[i], buttonArray[i], buttonArray[i + 1]);
-// 			}
-// 			else {
-// 				hide(listArray[i], submitContentArray[i], buttonArray[i], buttonArray[i + 1])
-// 			}
-// 		}
-// 	}
