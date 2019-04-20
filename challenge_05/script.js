b3ba1e812a9b90d9b7211f43f4ffe45e04e3ec83
Nicole Bartholomew <nabartholomew@gmail.com>
2019-04-20 11:42:43 -0500

    (feat) Created function that rotates through wordArray with proper timing.

--- challenge_05/script.js
+++ challenge_05/script.js
@@ -1,16 +1,10 @@
 document.addEventListener("DOMContentLoaded", function(event){
 	let wordArray = ["design", "frontend", "backend", "testing"];
 	let wordSpan = document.getElementsByClassName("word")[0];
-
-	function changeWord (){
-		for (let word of wordArray){
-
-			function nextWord () {
-				wordSpan.textContent = word;
-			}
-			setTimeout(nextWord, 3000);
-		}
-	}
-
-	changeWord();
+	let currentWord = 0;
+	
+	let changeWordId = setInterval(function() {
+		wordSpan.textContent = wordArray[currentWord];
+		currentWord++;
+	}, 2000);
 });
