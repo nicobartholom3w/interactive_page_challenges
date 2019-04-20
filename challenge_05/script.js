abc79ca41d3c61eb374b7f6d8c4a7a0ed4191f80
Nicole Bartholomew <nabartholomew@gmail.com>
2019-04-19 19:58:41 -0500

    (feat) Added function to change word. Added width to .main.

--- challenge_05/script.js
+++ challenge_05/script.js
@@ -1,5 +1,16 @@
 document.addEventListener("DOMContentLoaded", function(event){
 	let wordArray = ["design", "frontend", "backend", "testing"];
+	let wordSpan = document.getElementsByClassName("word")[0];
 
+	function changeWord (){
+		for (let word of wordArray){
 
+			function nextWord () {
+				wordSpan.textContent = word;
+			}
+			setTimeout(nextWord, 3000);
+		}
+	}
+
+	changeWord();
 });
