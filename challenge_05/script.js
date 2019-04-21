7479bc96eef8bb4c9d62469159e6024fd5dae3e6
Nicole Bartholomew <nabartholomew@gmail.com>
2019-04-21 13:07:00 -0500

    (feat) First attempt at typewriter effect using two setIntervals. Additional notes on future direction.

--- challenge_05/script.js
+++ challenge_05/script.js
@@ -2,9 +2,18 @@ document.addEventListener("DOMContentLoaded", function(event){
 	let wordArray = ["design", "frontend", "backend", "testing"];
 	let wordSpan = document.getElementsByClassName("word")[0];
 	let currentWord = 0;
+	let currentLetter = 0;
 	
 	let changeWordId = setInterval(function() {
-		wordSpan.textContent = wordArray[currentWord];
+		let changeLetterId = setInterval(function() {
+			let thisWord = wordArray[currentWord];
+			thisWordArray = thisWord.split("");
+			wordSpan.textContent = thisWordArray[currentLetter];
+			currentLetter++;
+		}, 2000);
 		currentWord++;
+		if(currentWord == 4){
+			currentWord = 0;
+		}
 	}, 2000);
 });
