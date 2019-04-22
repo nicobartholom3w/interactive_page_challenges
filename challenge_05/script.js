64642c65559ba41544c6da4f7a79e47fdd34bb5b
Nicole Bartholomew <nabartholomew@gmail.com>
2019-04-22 17:27:24 -0500

    (feat) Switched over to using setTimeout rather than setInterval.

--- challenge_05/script.js
+++ challenge_05/script.js
@@ -1,19 +1,22 @@
 document.addEventListener("DOMContentLoaded", function(event){
 	let wordArray = ["design", "frontend", "backend", "testing"];
 	let wordSpan = document.getElementsByClassName("word")[0];
-	let currentWord = 0;
-	let currentLetter = 0;
-	
-	let changeWordId = setInterval(function() {
-		let changeLetterId = setInterval(function() {
-			let thisWord = wordArray[currentWord];
-			thisWordArray = thisWord.split("");
-			wordSpan.textContent = thisWordArray[currentLetter];
-			currentLetter++;
-		}, 2000);
-		currentWord++;
-		if(currentWord == 4){
-			currentWord = 0;
+	let wordSoFar = "";
+	// change word when finished going through letters in current word
+	// 
+	for(let word of wordArray){
+		let wordSplit = word.split("");
+		let letterCount = 0;
+		spellOutWord(wordSplit, letterCount);
+	}
+
+	function spellOutWord(wordSplit, letterCount) {
+		let currentLetter = wordSplit[letterCount];
+		wordSoFar += currentLetter;
+		wordSpan.textContent = wordSoFar;
+		letterCount++;
+		if(letterCount < wordSplit.length){
+			setTimeout(spellOutWord, 1000);
 		}
-	}, 2000);
+	}
 });
