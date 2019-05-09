f45f37e7459da182a11e3f6a3bdd4c16dd8fea0e
Nicole Bartholomew <nabartholomew@gmail.com>
2019-05-09 16:08:29 -0500

    (feat) Fixed positioning of card element and have basic rotating functionality.

--- challenge_06/script.js
+++ challenge_06/script.js
@@ -1,43 +1,68 @@
 document.addEventListener("DOMContentLoaded", function(event){
 	let container = document.getElementsByClassName("container")[0];
 	let card = document.getElementsByClassName("card")[0];
-	let counter = 0;
-	let updateRate = 10;
-	let isTimeToUpdate = function() {
-	  return counter++ % updateRate === 0;
-	};
 
 	let mouse = {
 		_x: 0,
 		_y: 0,
 		x: 0,
 		y: 0,
-		setOrigin: function(e) {
-			this._x = e.offsetLeft + Math.floor(e.offsetWidth/2);
-			this._y = e.offsetTop + Math.floor(e.offsetHeight/2);
-		},
-		setPosition: function(event) {
+		updatePosition: function(event) {
 			let e = event || window.event;
 			this.x = e.clientX - this._x;
 			this.y = (e.clientY - this._y) * -1;
 		},
-		show: function() {
-			return '(' + this.x + ', ' + this.y + ')';
-		} 
+		setOrigin: function(e) {
+			this._x = e.offsetLeft + Math.floor(e.offsetWidth/2);
+			this._y = e.offsetTop + Math.floor(e.offsetHeight/2);
+		},
+		show: function() { return '(' + this.x + ', ' + this.y + ')'; }
 	}
 
-	
 
 	mouse.setOrigin(container);
-	// record location of mouse on container/card
-	// 	set center of card as 0
-		
 
+	let onMouseEnterHandler = function(event) {
+		update(event);
+	};
 
+	let onMouseLeaveHandler = function() {
+		card.style = "";
+	};
 
+	let onMouseMoveHandler = function(event) {
+		if(isTimeToUpdate()) {
+			update(event);
 
+		}
+	};
 
+	let update = function(event) {
+		mouse.updatePosition(event);
+		updateTransformStyle(
+			(mouse.y / container.offsetHeight/2).toFixed(2),
+			(mouse.x / container.offsetWidth/2).toFixed(2)
+		);
+	};
 
+	let updateTransformStyle = function(x, y) {
+		let style = "rotateX(" + x + "deg) rotateY(" + y + "deg)";
+		card.style.transform = style;
+	}
+
+	container.onmouseenter = onMouseEnterHandler;
+	container.onmouseleave = onMouseLeaveHandler;
+	container.onmousemove = onMouseMoveHandler;
+
+	let counter = 0;
+	let updateRate = 10;
+	let isTimeToUpdate = function() {
+		// return counter++ % updateRate === 0;
+		return true;
+	}
 
 
 });
+
+
+
