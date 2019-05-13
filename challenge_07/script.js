5471a7078882e087a526a038d8de9d695de02586
Nicole Bartholomew <nabartholomew@gmail.com>
2019-05-13 12:02:10 -0500

    (feat) Added fade-in effects for container and cards.

--- challenge_07/script.js
+++ challenge_07/script.js
@@ -1,4 +1,32 @@
 document.addEventListener("DOMContentLoaded", function(event){
+	let cards = Array.from(document.getElementsByClassName("card"));
+	let container = document.getElementsByClassName("container")[0];
+
+	function cardFadeIn() {
+		let viewWidth = window.innerWidth || document.documentElement.clientWidth;
+		let viewHeight = window.innerHeight || document.documentElement.clientHeight;
+		for (let card of cards) {
+			let cardBounding = card.getBoundingClientRect();
+			if (cardBounding.top >= 0 && cardBounding.left >= 0 && cardBounding.top <= viewHeight){
+				card.className.replace("hidden", "card__fade-in");
+			}
+		}
+	} 
+
+	function hideExtraCards() {
+		for (let card of cards) {
+			let cardBounding = card.getBoundingClientRect();
+			if (cardBounding.top <= 0 && cardBounding.left <= 0 && cardBounding.top >= viewHeight){
+				card.classList.add("hidden");
+			}
+	}
+
+	window.addEventListener("scroll", cardFadeIn);
+
+	window.onload = function() {
+		container.className = container.className.replace("hidden", "container__fade-in");
+		hideExtraCards();
+	}
 
 
 
