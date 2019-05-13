d9b2519fec099ebdc9693a82d83c8d2160463509
Nicole Bartholomew <nabartholomew@gmail.com>
2019-05-13 16:27:49 -0500

    (feat) Completed initial solution.

--- challenge_07/script.js
+++ challenge_07/script.js
@@ -1,17 +1,12 @@
 document.addEventListener("DOMContentLoaded", function(event){
 	let cards = Array.from(document.getElementsByClassName("card"));
 	let container = document.getElementsByClassName("container")[0];
+	let hiddenCards = [];
 
-	function cardFadeIn() {
-		let viewWidth = window.innerWidth || document.documentElement.clientWidth;
-		let viewHeight = window.innerHeight || document.documentElement.clientHeight;
-		for (let card of cards) {
-			let cardBounding = card.getBoundingClientRect();
-			if (cardBounding.left >= 0 && cardBounding.top <= viewHeight){
-				card.className.replace("hidden", "card__fade-in");
-			}
-		}
-	} 
+	function initialLoad () {
+		container.className = container.className.replace("hidden", "container__fade-in");
+		hideExtraCards();
+	}
 
 	function hideExtraCards() {
 		let viewWidth = window.innerWidth || document.documentElement.clientWidth;
@@ -20,22 +15,85 @@ document.addEventListener("DOMContentLoaded", function(event){
 			let cardBounding = card.getBoundingClientRect();
 			if (cardBounding.left >= 0 && cardBounding.top >= viewHeight){
 				card.classList.add("hidden");
+				hiddenCards.push(card);
+
 			}
 		}
-		// for (let card of cards) {
-		// 	let cardBounding = card.getBoundingClientRect();
-		// 	if (cardBounding.top <= 0 && cardBounding.left <= 0 && cardBounding.top >= viewHeight){
-		// 		card.classList.add("hidden");
-		// 	}
 	}
 
+
+	let i = 0;
+	function cardFadeIn() {
+		i++;
+		console.log("scrolled " + i + " times");
+		
+		let viewHeight = window.innerHeight || document.documentElement.clientHeight;
+		for (let hiddenCard of hiddenCards) {
+			let hiddenCardBounding = hiddenCard.getBoundingClientRect();
+			if (hiddenCardBounding.top <= viewHeight){
+				hiddenCard.classList.remove("hidden");
+				hiddenCard.classList.add("card__fade-in");
+				// hiddenCard.className.replace("hidden", "card__fade-in");
+				hiddenCards.shift(hiddenCard);
+				console.log(hiddenCard);
+			}
+		}
+	} 
+
+	initialLoad();
 	window.addEventListener("scroll", cardFadeIn);
-
-	window.onload = function() {
-		container.className = container.className.replace("hidden", "container__fade-in");
-		hideExtraCards();
-	}
-
+	
 
 
 });
+
+
+
+
+
+	// function inViewport(element) {
+	// 	let viewHeight = window.innerHeight || document.documentElement.clientHeight;
+	// 	let elementPosition = element.getBoundingClientRect();
+	// 	return (elementPosition.top <= viewHeight);
+	// }
+	// 
+	// 
+	// 
+	// 
+	// document.addEventListener('scroll', event => {
+	// 	let viewWidth = window.innerWidth || document.documentElement.clientWidth;
+	// 	let viewHeight = window.innerHeight || document.documentElement.clientHeight;
+	// 	for (let card of cards) {
+	// 		let cardBounding = card.getBoundingClientRect();
+	// 		if (cardBounding.left >= 0 && cardBounding.top <= viewHeight){
+	// 			card.className.replace("hidden", "card__fade-in");
+	// 		}
+	// 	}
+	// });
+
+	
+
+
+	// window.onscroll = function () {
+	// 	let viewHeight = window.innerHeight || document.documentElement.clientHeight;
+	// 	for (let hiddenCard of hiddenCards) {
+	// 		let hiddenCardBounding = hiddenCard.getBoundingClientRect();
+	// 		if (hiddenCardBounding.top <= viewHeight){
+	// 			hiddenCard.className.replace("hidden", "card__fade-in");
+	// 		}
+	// 	}
+	// }
+
+	
+
+	// window.onscroll = cardFadeIn;
+	// 
+	// 
+	// 
+	// 
+	// 
+	// 				// window.addEventListener("scroll", event => {
+				// 	if(cardBounding.top <= viewHeight) {
+				// 		card.className.replace("hidden", "card__fade-in");
+				// 	}
+				// });
