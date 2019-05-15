0f921c01b50636ad604e339c5093fcf467b56742
Nicole Bartholomew <nabartholomew@gmail.com>
2019-05-15 11:24:47 -0500

    (feat) Initial solution for challenge completed.

--- challenge_08/script.js
+++ challenge_08/script.js
@@ -8,37 +8,45 @@ document.addEventListener("DOMContentLoaded", function(event){
 	let sideMenuRun = document.querySelector(".side__menu-run");
 	let overlay = document.querySelector(".right-overlay");
 
-	searchButton.addEventListener("click", (event) => {
+	function addOverlay () {
 		sideMenu.classList.remove("slide-out");
 		sideMenu.classList.add("slide-in");
-		sideMenuSearch.classList.remove("side__menu-search-non-active")
-		sideMenuSearch.classList.add("side__menu-search-active");
 		overlay.classList.add("right-overlay-active");
-		overlay.classList.add("right-overlay-zindex")
+		overlay.classList.add("right-overlay-zindex");
+	}
+
+	function removeOverlay (){
+		sideMenu.classList.remove("slide-in");
+		sideMenu.classList.add("slide-out");
+		overlay.classList.remove("right-overlay-active");
+		setTimeout(() => overlay.classList.remove("right-overlay-zindex"), 600);
+	}
+
+	searchButton.addEventListener("click", (event) => {
+		sideMenuSearch.classList.add("side__menu-search-active");
+		addOverlay();
 	})
 
 	runButton.addEventListener("click", (event) => {
-		sideMenu.classList.add("slide-in");
 		sideMenuRun.classList.add("side__menu-run-active");
-		overlay.classList.add("right-overlay-active");
+		addOverlay();
 	})
 
 	hideButton.addEventListener("click", (event) => {
-		sideMenu.classList.remove("slide-in");
-		sideMenu.classList.add("slide-out");
-		overlay.classList.remove("right-overlay-active");
 		setTimeout(() => sideMenuSearch.classList.remove("side__menu-search-active"), 500);
-		setTimeout(() => overlay.classList.remove("right-overlay-zindex"), 600);
+		removeOverlay();
 	})
 	
 	closeButton.addEventListener("click", (event) => {
-		sideMenu.classList.remove("slide-in");
-		sideMenu.classList.add("slide-out");
-		sideMenuRun.classList.remove("side__menu-run-active");
-		overlay.classList.remove("right-overlay-active");
+		setTimeout(() => sideMenuRun.classList.remove("side__menu-run-active"), 500);
+		removeOverlay();
 	})
 
-
+	overlay.addEventListener("click", (event) => {
+		setTimeout(() => sideMenuSearch.classList.remove("side__menu-search-active"), 500);
+		setTimeout(() => sideMenuRun.classList.remove("side__menu-run-active"), 500);
+		removeOverlay();
+	})
 
 
 });
