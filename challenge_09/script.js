d4df8607c451fd8bc66c14ef70137f53dd0654b6
Nicole Bartholomew <nabartholomew@gmail.com>
2019-05-22 15:59:25 -0500

    (feat) Completed non-sticky challenge.

--- challenge_09/script.js
+++ challenge_09/script.js
@@ -1,5 +1,16 @@
 document.addEventListener("DOMContentLoaded", function(event){
+	
+	let card = document.querySelector(".card");
+	let sticky = card.offsetTop;
 
+	window.onscroll = () => {
+		if (window.pageYOffset >= sticky) {
+			card.classList.add("sticky");
+		}
+		else {
+			card.classList.remove("sticky");
+		}
+	}
 
 
 
