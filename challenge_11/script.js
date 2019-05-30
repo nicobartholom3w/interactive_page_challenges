7040ba01a4da3af5e233376e3b1f4963b6aa42fb
Nicole Bartholomew <nabartholomew@gmail.com>
2019-05-29 20:02:08 -0500

    (feat) Added initializeForm function.

--- challenge_11/script.js
+++ challenge_11/script.js
@@ -1,4 +1,22 @@
 document.addEventListener("DOMContentLoaded", function(event){
+	let inputArray = document.getElementsByClassName("form__input");
+	initializeForm();
+
+
+	function initializeForm() {
+		for(let i = 0; i < inputArray.length; i++) {
+			let inputBox = inputArray[i];
+			if(i == 0) {
+				inputBox.focus();
+			}
+			else {
+				inputBox.readOnly = true;	
+			}
+		}
+	}
+
+
+
 
 
 
