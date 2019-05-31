8d176dc3292e639df8009fc9c1750fd0f6813e24
Nicole Bartholomew <nabartholomew@gmail.com>
2019-05-31 15:26:36 -0500

    (feat) deleteback function working for returning to previous input field on keydown delete.

--- challenge_11/script.js
+++ challenge_11/script.js
@@ -5,15 +5,15 @@ document.addEventListener("DOMContentLoaded", function(event){
 		for(let i = 0; i < inputArray.length; i++) {
 			let inputBox = inputArray[i];
 			inputBox.maxLength = "1";
+			inputBox.setAttribute("oninput", "autotab(this, document.code.box" + (i + 2) + ")");
+			inputBox.setAttribute("onkeydown", "deleteback(document.code.box" + i + ", this, event)");
 			if(i == 0) {
 				inputBox.focus();
 			}
 			else {
 				inputBox.readOnly = "true";	
 			}
-			if(i < inputArray.length - 1){
-				inputBox.setAttribute("oninput", "autotab(this, document.code.box" + (i + 2) + ")");
-			}
+				
 		}
 	}
 
@@ -31,15 +31,27 @@ document.addEventListener("DOMContentLoaded", function(event){
 
 	document.onmousedown = preventFocus;
 	document.onkeydown = preventFocusTab;
+
+
 });
 
 function autotab(current, to) {
 	if (current.getAttribute && current.value.length == current.getAttribute("maxlength")) {
-    	to.focus();
-    	to.readOnly = false; 
-    }
+		to.focus();
+		to.readOnly = false; 
+	}
+
+  
 }
 
-
+function deleteback(previous, current, event) {
+    if(current.value.length == 0 && event.keyCode == 8) {
+    	current.blur();
+    	current.readOnly = true;
+    	previous.focus();
+    	previous.value = "";
+    	previous.readOnly = false;
+	}
+}
 
 
