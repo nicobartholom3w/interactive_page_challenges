e70a110a410de7a2b6f4eeb346fb4fa72ec37632
Nicole Bartholomew <nabartholomew@gmail.com>
2019-05-31 13:32:10 -0500

    (feat) Completed functioning autotab function.

--- challenge_11/script.js
+++ challenge_11/script.js
@@ -1,8 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(event){
 	let inputArray = document.getElementsByClassName("form__input");
-	initializeForm();
-
-
+	
 	function initializeForm() {
 		for(let i = 0; i < inputArray.length; i++) {
 			let inputBox = inputArray[i];
@@ -11,22 +9,20 @@ document.addEventListener("DOMContentLoaded", function(event){
 				inputBox.focus();
 			}
 			else {
-				inputBox.readOnly = true;	
+				inputBox.readOnly = "true";	
+			}
+			if(i < inputArray.length - 1){
+				inputBox.setAttribute("oninput", "autotab(this, document.code.box" + (i + 2) + ")");
 			}
 		}
 	}
 
-	function autoTab(current, to) {
-		if (current.getAttribute && current.value.length==current.getAttribute("maxlength")) {
-        	to.focus(); 
-    	}
-	}
-
-
-document.oninput = autoTab;
-
-
-
-
-
+	initializeForm();
 });
+
+function autotab(current, to) {
+	if (current.getAttribute && current.value.length == current.getAttribute("maxlength")) {
+    	to.focus();
+    	to.readOnly = false; 
+    }
+}
