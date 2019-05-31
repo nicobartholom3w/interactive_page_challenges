0f636abc773456ffd01e2cd3fe3658bde66f1055
Nicole Bartholomew <nabartholomew@gmail.com>
2019-05-31 17:04:10 -0500

    (feat) Finished adding proper on and off clicking for selection of appropriate input field.

--- challenge_11/script.js
+++ challenge_11/script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(event){
 	let inputArray = Array.from(document.getElementsByClassName("form__input"))
-	let submitButton = document.querySelector(".form__submit");
+	let fieldSet = document.querySelector(".form__fieldset");
 	
 	function initializeForm() {
 		for(let i = 0; i < inputArray.length; i++) {
@@ -8,8 +8,9 @@ document.addEventListener("DOMContentLoaded", function(event){
 			inputBox.maxLength = "1";
 			inputBox.setAttribute("oninput", "autotab(this, document.code.box" + (i + 2) + ")");
 			inputBox.setAttribute("onkeydown", "deleteback(document.code.box" + i + ", this, event)");
+			inputBox.setAttribute("focus", "false");
 			if(i == 0) {
-				inputBox.focus();
+				inputBox.setAttribute("focus", "true");
 			}
 			else {
 				inputBox.readOnly = "true";	
@@ -28,6 +29,21 @@ document.addEventListener("DOMContentLoaded", function(event){
 		}
 	}
 
+	function setFocus(event) {
+		for(let element of inputArray) {
+			if(event.target.parentNode == fieldSet) {
+				if(element.getAttribute("focus") == "true") {
+					element.focus();
+				}
+			}
+			else {
+				element.blur();
+			}
+		}
+		
+		
+	}
+
 	function alert(event) {
 		let valueArray = [];
 		for(let element of inputArray) {
@@ -43,11 +59,13 @@ document.addEventListener("DOMContentLoaded", function(event){
 	document.onmousedown = preventFocus;
 	document.onkeydown = preventFocusTab;
 	document.onsubmit = alert;
-
+	document.onclick = setFocus;
 });
 
 function autotab(current, to) {
 	if (current.getAttribute && current.value.length == current.getAttribute("maxlength")) {
+		current.setAttribute("focus", "false");
+		to.setAttribute("focus", "true");
 		to.focus();
 		to.readOnly = false; 
 	}
@@ -57,6 +75,8 @@ function autotab(current, to) {
 
 function deleteback(previous, current, event) {
     if(current.value.length == 0 && event.keyCode == 8) {
+    	current.setAttribute("focus", "false");
+    	previous.setAttribute("focus", "true");
     	current.blur();
     	current.readOnly = true;
     	previous.focus();
@@ -65,4 +85,7 @@ function deleteback(previous, current, event) {
 	}
 }
 
-
+function focus(current, fieldset) {
+	current.focus();
+	current.readOnly = false;
+}
