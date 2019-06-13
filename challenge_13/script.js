9645bf7950955d9d31ec399cb1be570dddf7693a
Nicole Bartholomew <nabartholomew@gmail.com>
2019-06-12 21:31:22 -0500

    (feat) Started adding javascript functionality for side scrolling.

--- challenge_13/script.js
+++ challenge_13/script.js
@@ -1,6 +1,21 @@
 document.addEventListener("DOMContentLoaded", function(event){
+	let scrollButtons = Array.from(document.getElementsByClassName("scroll-button"));
+	let scrollRightButton = scrollButtons[1];
+	let scrollLeftButton = scrollButtons[0];
+	let scrollBox = document.querySelector(".container-content");
+	let blocksArray = Array.from(document.getElementsByClassName("block"));
 
+	function scrollToRight(event) {
+		// get width of first element visible from left side
+		// blocksArray[i].offsetWidth
+		scrollBox.scrollRight
+	}
 
+	function scrollToLeft(event) {
+		scrollBox.scrollLeft
+	}
 
+	document.onclick = scrollToRight;
+	document.onclick = scrollToLeft;
 
 });
