29a85701abcd1776dda96b73ccfe0294ecbc19f5
Nicole Bartholomew <nabartholomew@gmail.com>
2019-06-24 17:05:29 -0500

    (feat) Initial functionality and display of timer is funcitonal.

--- challenge_16/script.js
+++ challenge_16/script.js
@@ -1,22 +1,22 @@
 document.addEventListener("DOMContentLoaded", function(event){
 	let bubbleArea = document.querySelector(".bubble-area");
 	let formInput = document.querySelector(".form__input");
-	let bubble;
 
 	function createBubble(event) {
 		event.preventDefault();
-		bubble = document.createElement("div");
+		let bubble = document.createElement("div");
 		bubble.classList.add("bubble");
 		bubbleArea.appendChild(bubble);
 		let time = Number.parseInt(formInput.value);
 		bubble.innerText = time;
 		setInterval(countDown, 1000, time, bubble);
-		// let intervalId = setInterval(bubblePop, time * 1000);
+		// bubblePop(bubble);
 	}
 
 	function countDown(time, bubble) {
 		if(time <= 0) {
-			return bubblePop();
+			bubblePop(bubble);
+			return;
 		}
 		time--;
 		bubble.innerText = time;
@@ -24,8 +24,11 @@ document.addEventListener("DOMContentLoaded", function(event){
 
 	}
 
-	function bubblePop() {
-
+	function bubblePop(bubble) {
+		bubble.style.animation = "fade-out .5s";
+		setInterval(() => {
+			bubbleArea.removeChild(bubble);
+		}, 500)
 	}
 
 	document.onsubmit = createBubble;
