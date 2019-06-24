02f33b7426ccf95bfec82baf9187dbc7a13b7f31
Nicole Bartholomew <nabartholomew@gmail.com>
2019-06-24 15:23:50 -0500

    (feat) Added css animationa and makeBubble function.

--- challenge_16/script.js
+++ challenge_16/script.js
@@ -1,5 +1,11 @@
 document.addEventListener("DOMContentLoaded", function(event){
+	let bubbleArea = document.querySelector(".bubble-area");
+	let bubble;
 
+	function createBubble() {
+		bubble = document.createElement("div");
+		bubble.classList.add("bubble");
+		bubbleArea.appendChild(bubble);
+	}
 
-	
 });
