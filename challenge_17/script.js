3aeb395c2b6a761bc6e0b657c5becdf5db7ee743
Nicole Bartholomew <nabartholomew@gmail.com>
2019-06-25 12:17:31 -0500

    (feat) Initial solution for custom scrollbar implementation.

--- challenge_17/script.js
+++ challenge_17/script.js
@@ -1,6 +1,14 @@
 document.addEventListener("DOMContentLoaded", function(event){
+	let cardContent = document.querySelector(".card__content");
+	let scrollContainer = document.querySelector(".scroll");
+	let scrollBar = document.querySelector(".scroll-bar");
+	scrollBar.style.height = scrollContainer.clientHeight * cardContent.clientHeight / cardContent.scrollHeight + "px";
 
+	function scrollIt(event) {
+		console.log("apple");
+	}
 
-
-
+	cardContent.addEventListener("scroll", (event) => {
+		scrollBar.style.top = scrollContainer.clientHeight * cardContent.scrollTop / cardContent.scrollHeight + "px";
+	})
 });
