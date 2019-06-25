f081efcd41cbaff45e866aba5128652a71f61abc
Nicole Bartholomew <nabartholomew@gmail.com>
2019-06-25 12:31:31 -0500

    (feat) Added resize eventlistener and on page load to properly size the scrollbar.

--- challenge_17/script.js
+++ challenge_17/script.js
@@ -4,11 +4,20 @@ document.addEventListener("DOMContentLoaded", function(event){
 	let scrollBar = document.querySelector(".scroll-bar");
 	scrollBar.style.height = scrollContainer.clientHeight * cardContent.clientHeight / cardContent.scrollHeight + "px";
 
-	function scrollIt(event) {
-		console.log("apple");
+	if(scrollBar.clientHeight === scrollContainer.clientHeight) {
+		scrollBar.style.opacity = "0";
+		scrollContainer.style.opacity = "0";
 	}
 
 	cardContent.addEventListener("scroll", (event) => {
 		scrollBar.style.top = scrollContainer.clientHeight * cardContent.scrollTop / cardContent.scrollHeight + "px";
+	})
+
+	document.addEventListener("resize", (event) => {
+		scrollBar.style.height = scrollContainer.clientHeight * cardContent.clientHeight / cardContent.scrollHeight + "px";	
+		if(scrollBar.clientHeight === scrollContainer.clientHeight) {
+			scrollBar.style.opacity = "0";
+			scrollContainer.style.opacity = "0";
+		}
 	})
 });
