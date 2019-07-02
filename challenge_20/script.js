4222731380d4892896e936108b04de7f0466efab
Nicole Bartholomew <nabartholomew@gmail.com>
2019-07-02 11:45:48 -0500

    (feat) Cleaned up some css hover issues.

--- challenge_20/script.js
+++ challenge_20/script.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(event){
-
+	let optionsArray = Array.from(document.getElementsByClassName("option"));
+	let optionsDescriptionArray = Array.from(document.getElementsByClassName("option-description"));
 
 
 });
