77cf8ca0d5d1ccd800f7d0861fffc7a16d584f7f
Nicole Bartholomew <nabartholomew@gmail.com>
2019-07-14 19:12:16 -0500

    (feat) Changed code to take in dataset information for alerts from buttons.

--- challenge_24/script.js
+++ challenge_24/script.js
@@ -1,6 +1,4 @@
 document.addEventListener("DOMContentLoaded", function(event){
-	let message = document.querySelector(".toast__message");
-	let alert = document.querySelector(".toast__alert");
 	let messageButton = document.querySelector(".button__message");
 	let alertButton = document.querySelector(".button__alert");
 	let busy = false;
@@ -8,29 +6,46 @@ document.addEventListener("DOMContentLoaded", function(event){
 	function showToast(element){
 		if(!busy) {
 			busy = true;
-			element.classList.add("toast-active");
+			setTimeout(() => {element.classList.add("toast-active")}, 100);
 			let mouseOverTimeout = setTimeout(() => {removeToast(element);}, 5000);
 			element.addEventListener("mouseover", (event) => {
 				clearTimeout(mouseOverTimeout);
 			});
 			element.addEventListener("mouseout", (event) => {
 				mouseOverTimeout = setTimeout(() => {removeToast(element);}, 5000);
-			})
+			});
 		}
 	}
 
 	function removeToast(element) {
 		element.classList.remove("toast-active");
-
-		setTimeout(() => {busy = false;}, 200);
+		setTimeout(() => {
+			busy = false;
+			element.remove();
+		}, 200);
 	}
 
+	function createToast(button) {
+		let div = document.createElement("div");
+		div.classList.add("toast");
+		div.innerText = button.dataset.text;
+		if(button.dataset.type === "error") {
+			div.classList.add("toast__alert");
+		}
+		else {
+			div.classList.add("toast__message");
+		}
+		document.body.appendChild(div);
+		return div;
+	}
 
 	messageButton.addEventListener("click", (event) => {
-		showToast(message);
+		let e = createToast(event.target);
+		showToast(e);
 	});
 
 	alertButton.addEventListener("click", (event) => {
-		showToast(alert);
+		let e = createToast(event.target);
+		showToast(e);
 	});
 });
