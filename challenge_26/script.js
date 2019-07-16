d162dd8efbc0b1fe59d2fb89159120722e2ce6bc
Nicole Bartholomew <nabartholomew@gmail.com>
2019-07-16 18:58:36 -0500

    (feat) Created new working solution for challenge.

--- challenge_26/script.js
+++ challenge_26/script.js
@@ -9,35 +9,64 @@ document.addEventListener("DOMContentLoaded", function(event){
 	});
 
 	function startOnboarding(event) {
-		continueOnboarding(event, itemArray[step]);
+		overlay.classList.add("overlay__active");
+		activateElement(event, itemArray[step]);
+		// continueOnboarding(event, itemArray[step]);
 	}
 
-	function continueOnboarding(event, activeElement, priorElement){
-		event.stopPropagation();
-		event.preventDefault();
-		if(priorElement) {
-			priorElement.style.zIndex = "0";
-			priorElement.style.cursor = "default";
-		}
-		if(step >= itemArray.length) {
+	// function continueOnboarding(event, activeElement, priorElement){
+	// 	event.stopPropagation();
+	// 	event.preventDefault();
+	// 	if(priorElement) {
+	// 		priorElement.style.zIndex = "0";
+	// 		priorElement.style.cursor = "default";
+	// 	}
+	// 	if(step >= itemArray.length) {
+	// 		overlay.classList.remove("overlay__active");
+	// 		target.element.removeEventListener("click", nextElement);
+	// 		step = 0;
+	// 		return;
+	// 	}
+
+	// 	overlay.classList.add("overlay__active");
+	// 	activeElement.style.zIndex = "2";
+	// 	activeElement.style.cursor = "pointer";
+	// 	step++;
+
+	// 	function nextElement(event) {
+	// 		event.stopPropagation();
+	// 		event.preventDefault();
+	// 		let prevElement = activeElement;
+	// 		let currentElement = itemArray[step];
+	// 		continueOnboarding(event, currentElement, prevElement);
+	// 	}
+	// 	activeElement.addEventListener("click", nextElement);
+	// 	target.element.removeEventListener("click", nextElement);
+	// }
+	// 
+	function activateElement(event, element) {
+		element.style.zIndex = "2";
+		element.style.cursor = "pointer";
+		step++;
+		element.addEventListener("click", nextElement);
+	}
+
+	function nextElement(event) {
+		if(step > 2) {
+			event.target.removeEventListener("click", nextElement);
 			overlay.classList.remove("overlay__active");
+			event.target.style.zIndex = "0";
+			event.target.style.cursor = "default";
 			step = 0;
 			return;
 		}
+		event.target.removeEventListener("click", nextElement);
+		event.target.style.zIndex = "0";
+		event.target.style.cursor = "default";
 
-		overlay.classList.add("overlay__active");
-		activeElement.style.zIndex = "2";
-		activeElement.style.cursor = "pointer";
+		itemArray[step].style.zIndex = "2";
+		itemArray[step].style.cursor = "pointer";
+		itemArray[step].addEventListener("click", nextElement);
 		step++;
-
-		function nextElement(event) {
-			event.stopPropagation();
-			event.preventDefault();
-			let prevElement = activeElement;
-			let currentElement = itemArray[step];
-			continueOnboarding(event, currentElement, prevElement);
-		}
-		activeElement.addEventListener("click", nextElement, false);
-		target.element.removeEventListener("click", nextElement, false);
 	}
 });
