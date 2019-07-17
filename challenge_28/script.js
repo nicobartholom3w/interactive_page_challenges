038c422c556266ccbc99d0b03ec323005126de6c
Nicole Bartholomew <nabartholomew@gmail.com>
2019-07-17 17:12:42 -0500

    (feat) Fixed issues with dropdown display and can bring up pokemon that start with the first letter typed into input.

--- challenge_28/script.js
+++ challenge_28/script.js
@@ -1,8 +1,27 @@
 document.addEventListener("DOMContentLoaded", function(event){
 	let input = document.querySelector(".input");
-
+	let dropDown = document.querySelector(".dropdown");
+	let keyCount = 0;
+	
 	input.addEventListener("keydown", (event) => {
-
+		dropDown.classList.add("dropdown-active");
+		fetch("https://pokeapi.co/api/v2/pokemon/?limit=964")
+		.then((response) => response.json())
+		.then((data) => {
+			let pokemonObjectsArray = data.results;
+			for(let pokemon of pokemonObjectsArray){
+				let nthLetter = pokemon.name[keyCount];
+				if(event.key.toLowerCase() === nthLetter) {
+					let listItem = document.createElement("div");
+					listItem.classList.add("dropdown-li");
+					listItem.innerText = pokemon.name;
+					dropDown.appendChild(listItem);
+				}
+			}
+		});
+		// find out what key
+		// search api for names that begin with same letter
+		// display list of up to first 5 pokemon with same first letter
 	});
 
 });
