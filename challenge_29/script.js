8ce40be466e8d3ca87b1b1ec7560462f3a53e8ac
Nicole Bartholomew <nabartholomew@gmail.com>
2019-07-23 14:38:16 -0500

    (feat) Can highlight hashtag elements, but the highlighted text alignment is not correct.

--- challenge_29/script.js
+++ challenge_29/script.js
@@ -1,3 +1,47 @@
 document.addEventListener("DOMContentLoaded", function(event){
+	let textArea = document.getElementById("textarea");
+	let highlights = document.querySelector(".backdrop__highlights");
+	let backdrop = document.querySelector(".backdrop");
+	let isHashtag = false;
 
+	function checkKeyDown(event) {
+		if(event.key === "#") {
+			isHashtag = true;
+		}
+		if(isHashtag && (event.key === " ")) {
+			isHashtag = false;
+		}
+	}
+
+	function handleInput(event) {
+		let highlightedText = applyHighlights(textArea.value);
+		highlights.innerHTML = highlightedText;
+	}
+
+	function applyHighlights(text) {
+		text = text.replace(/\n$/gi, '\n\n');
+		let isHashRegex = /#\S+/gi;
+		// if(isHashRegex.exec(text)) {
+		// 	console.log("match");
+		// }
+		let matches = text.match(isHashRegex);
+		console.log(matches);
+		if(matches) {
+			for(let match of matches) {
+				console.log(match);
+				text = text.replace(match, "<mark>" + match + "</mark>");
+			}
+		}
+
+		return text;
+	}
+
+	function handleScroll(event) {
+		let scrollTop = textArea.scrollTop();
+		backdrop.scrollTop(scrollTop);
+	}
+
+	textArea.oninput = handleInput;
+	textArea.onscroll = handleScroll;
+	textArea.onkeydown = checkKeyDown;
 });
