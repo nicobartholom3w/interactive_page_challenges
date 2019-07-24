857e7ad8459333b6a9ace5ea38b30bb90ba1ba92
Nicole Bartholomew <nabartholomew@gmail.com>
2019-07-24 12:46:24 -0500

    (feat) Completed improved solution that solves scrolling and resizing issues.

--- challenge_29/script.js
+++ challenge_29/script.js
@@ -2,9 +2,6 @@ document.addEventListener("DOMContentLoaded", function(event){
 	let textArea = document.getElementById("textarea");
 	let highlights = document.querySelector(".backdrop__highlights");
 	let backdrop = document.querySelector(".backdrop");
-	let card = document.querySelector(".card");
-	let marks;
-	let mouseDown = false;
 
 	function handleInput(event) {
 		let highlightedText = applyHighlights(textArea.value);
@@ -15,7 +12,7 @@ document.addEventListener("DOMContentLoaded", function(event){
 		// check for newline
 		let alignmentRegex = /\n$/g;
 		text = text.replace(alignmentRegex, '\n\n');
-		// check for hash
+		// check for hashtag
 		let isHashRegex = /#[A-Za-z0-9_]*/g;
 		text = text.replace(isHashRegex, "<mark>$&</mark>");
 
@@ -23,38 +20,10 @@ document.addEventListener("DOMContentLoaded", function(event){
 	}
 
 	function handleScroll(event) {
-		let scrollTop = textArea.scrollTop();
-		backdrop.scrollTop(scrollTop);
-	}
-
-	function mouseDownHandler(event) {
-		mouseDown = true;
-	}
-
-	function mouseUpHandler(event) {
-		mouseDown = false;
-		card.style.height = textArea.clientHeight + "px";
-		backdrop.style.zIndex = "1";
-		textArea.style.backgroundColor = "transparent";
-			for(let mark of marks) {
-				mark.style.color = "transparent";
-			}
-	}
-
-	function resizeHandler(event) {
-		marks = Array.from(document.getElementsByTagName("mark"));
-		if(mouseDown === true) {
-			textArea.style.backgroundColor = "white";
-			backdrop.style.zIndex = "3";
-			for(let mark of marks) {
-				mark.style.color = "black";
-			}
-		}
+		let scroll = textArea.scrollTop;
+		backdrop.scrollTop = scroll;
 	}
 
 	textArea.oninput = handleInput;
 	textArea.onscroll = handleScroll;
-	textArea.onmousedown = mouseDownHandler;
-	textArea.onmousemove = resizeHandler;
-	document.onmouseup = mouseUpHandler;
 });
