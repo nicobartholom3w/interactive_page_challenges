c112e3ca013378c06efc0db5d3188f4d242e45ab
Nicole Bartholomew <nabartholomew@gmail.com>
2019-07-29 17:03:17 -0500

    (feat) Zoom lens active on hover and deactive when leave img area successfully.

--- challenge_30/script.js
+++ challenge_30/script.js
@@ -1,5 +1,45 @@
 document.addEventListener("DOMContentLoaded", function(event){
+	let imgContainer = document.querySelector(".img__container");
+	let img = document.querySelector(".img");
+	let imgZoom = document.querySelector(".img__zoom");
+	let lens;
+	let lensDiameter = 20;
+	let imgRect = img.getBoundingClientRect();
+	let lensActive = false;
 
+	function moveLens(event) {
+		if(event.target === img) {
+			addLens(event);
+		}
+		if(lensActive) {
+			lens.style.top = event.clientY - imgRect.top - lensDiameter + "px";
+			lens.style.left = event.clientX - imgRect.left - lensDiameter + "px";
+			if(event.clientX <= imgRect.left || event.clientX >= imgRect.right || event.clientY <= imgRect.top || event.clientY >= imgRect.bottom) {
+				removeZoom(event);
+			}
+		}
+	}
 
+	function addLens(event) {
+		if(event.relatedTarget === lens || lensActive === true) {
+			return;
+		}
+		lensActive = true;
+		lens = document.createElement("div");
+		lens.classList.add("hover__lens");
+		imgContainer.prepend(lens);
+		imgZoom.classList.add("img__zoom-active");
+		let rect = event.target.getBoundingClientRect();
+		lens.style.top = event.clientY - rect.top - lensDiameter + "px";
+		lens.style.left = event.clientX - rect.left - lensDiameter + "px";
+		lens.classList.add("hover__lens-active");
+	}
 
+	function removeZoom(event) {
+		imgZoom.classList.remove("img__zoom-active");
+		lens.classList.remove("hover__lens-active");
+		lensActive = false;
+	}
+
+	document.onmousemove = moveLens;
 });
